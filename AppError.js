9fea832bd420188dc5fb46dea97669ad85e9f596
Ninja <cuansenputra@gmail.com>
2023-02-23 16:32:47 +0700

    Error in Mongoose

--- AppError.js
+++ AppError.js
@@ -7,3 +7,5 @@ class AppError extends Error{
 }
 
 module.exports = AppError;
+
+
--- index.js
+++ index.js
@@ -24,9 +24,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand2')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 const categories = ['fruit', 'vegetable', 'dairy'];
+
 //route
 app.get('/products',  async (req, res, next) => {
     try{
@@ -41,11 +42,11 @@ app.get('/products',  async (req, res, next) => {
     }catch(err){
         next(e)
     }
-})
+});
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new', { categories })
-})
+    res.render('products/new', { categories });
+});
 
 app.post('/products', async (req, res, next) => {
     try{
@@ -55,56 +56,62 @@ app.post('/products', async (req, res, next) => {
     } catch(err){
         next(err)
     }       
-})  
+}); 
 
-app.get('/products/:id', async (req, res, next) => {
-    try{
+function wrapAsync(fn){
+    return function(req, res, next){
+        fn(req, res, next).catch(e => next(e))
+    }
+}
+
+
+app.get('/products/:id', wrapAsync (async (req, res, next) => {
         const { id } = req.params;
         const product = await Product.findById(id);
         if(!product){
             throw new AppError("Product Not Found", 404);
         }
         res.render('products/show', { product })
-    }catch(err){
-        next(err)
-    }
-})
+}))
 
-app.get('/products/:id/edit', async (req, res, next) => {
-    try{
+app.get('/products/:id/edit', wrapAsync(async (req, res, next) => {
         const { id } = req.params;
         const product = await Product.findById(id);
         if(!product){
             throw new AppError("Product Not Found", 404);
         }
         res.render('products/edit', { product, categories })
-    }catch(err){
-        next(err)
-    }
-})
+}));
 
-app.put('/products/:id', async (req, res, next) => {
-    try{
+app.put('/products/:id', wrapAsync(async (req, res, next) => {
         const { id } = req.params;
         const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
         res.redirect(`/products/${ product._id }`)    
-    }catch(err){
-        next(err)
-    }
-})
+}));
 
 app.delete('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findByIdAndDelete(id);
     res.redirect('/products');
+});
+
+const handleValidationErr = err => {
+    console.dir(err);
+    return new AppError(`Validation Failed... ${err.message}`, 400);
+}
+
+app.use((err, req, res, next) => {
+    console.log(err.name);
+    if(err.name === 'ValidationError') err = handleValidationErr(err)
+    next(err);
 })
 
 app.use((err, req, res, next) => {
     const { status = 500, message = "Something went wrong" } = err;
     res.status(status).send(message);
-})
+});
 
 //cof port
 app.listen(3000, () => {
-    console.log("LISTENING ON PORT 3000")
-})
+    console.log("LISTENING ON PORT 3000");
+});
